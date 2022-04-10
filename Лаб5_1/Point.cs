b614b5df1819750fb60fe408f0d05d87070172cc
MISTIKMAKS <mistikmaks80885@gmail.com>
2022-04-10 19:35:41 +0300

    first master commit

---
+++ Лаб5_1/Point.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Лаб5_1
+{
+    class Point
+    {
+        private double x;
+        private double y;
+
+        public Point()
+        {
+            x = 0;
+            y = 0;
+        }
+
+        public Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public Point(Point p)
+        {
+            this.x = p.x;
+            this.y = p.y;
+        }
+
+        public void SetX(double x)
+        {
+            this.x = x;
+        }
+
+        public void SetY(double y)
+        {
+            this.y = y;
+        }
+
+        public double GetX()
+        {
+            return this.x;
+        }
+
+        public double GetY()
+        {
+            return this.y;
+        }
+
+        //Переміщення точки по осі +>
+        public static Point operator -(Point p, double n)
+        {
+            p.x += n;
+            return p;
+
+        }
+
+        //Переміщення по осі +^
+        public static Point operator +(Point p, double n)
+        {
+            p.y += n;
+            return p;
+        }
+
+        //Визначення відстані між точками ->
+        public static double operator /(Point a, Point b)
+        {
+            double result = Math.Sqrt(Math.Pow((b.x - a.x), 2) + Math.Pow((b.y - a.y), 2));
+            return result;
+        }
+
+        //Операції порівняння точок ==
+        public static bool operator ==(Point a, Point b)
+        {
+            if (a.x == b.x && a.y == b.y)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        //Операції порівняння точок !=
+        public static bool operator !=(Point a, Point b)
+        {
+            if (a.x == b.x && a.y == b.y)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        public static Point operator --(Point p)
+        {
+            p.x--;
+            p.y--;
+            return p;
+        }
+
+        public static Point operator ++(Point p)
+        {
+            p.x++;
+            p.y++;
+            return p;
+        }
+
+        public override string ToString()
+        {
+            return " x: " + this.x + "\n y: " + this.y;
+        }
+    }
+}
---
+++ Лаб5_1/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Лаб5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("A_x: ");
+            double x_a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("A_y: ");
+            double y_a = Convert.ToDouble(Console.ReadLine()); 
+            Point a = new Point(x_a, y_a);
+            String result_a = a.ToString();
+            Console.WriteLine("A: ");
+            Console.WriteLine("{0}", result_a);
+
+            Console.Write("B_x: ");
+            double x_b = Convert.ToDouble(Console.ReadLine());
+            Console.Write("B_y: ");
+            double y_b = Convert.ToDouble(Console.ReadLine());
+            Point b = new Point(x_b, y_b);
+            String result_b = b.ToString();
+            Console.WriteLine("B: ");
+            Console.WriteLine("{0}", result_b);
+
+            Console.Write("n: ");
+            double n = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(" n: {0}", n);
+            Console.WriteLine("---------------");
+
+            a = a - n;
+            result_a = a.ToString();
+            Console.WriteLine("A +> n: ");
+            Console.WriteLine("{0}", result_a);
+            Console.WriteLine("---------------");
+
+            a = a + n;
+            result_a = a.ToString();
+            Console.WriteLine("A +^ n: ");
+            Console.WriteLine("{0}", result_a);
+            Console.WriteLine("---------------");
+
+            double result = a / b;
+            Console.WriteLine("A -> B: ");
+            Console.WriteLine("{0}", result);
+            Console.WriteLine("---------------");
+
+            bool boolean_is = a == b;
+            Console.WriteLine("A == B: ");
+            Console.WriteLine("{0}", boolean_is);
+            Console.WriteLine("---------------");
+
+            bool boolean_not = a != b;
+            Console.WriteLine("A != B: ");
+            Console.WriteLine("{0}", boolean_not);
+            Console.WriteLine("---------------");
+
+            a++;
+            result_a = a.ToString();
+            Console.WriteLine("A++: ");
+            Console.WriteLine("{0}", result_a);
+            Console.WriteLine("---------------");
+            a--;
+            result_a = a.ToString();
+            Console.WriteLine("A--: ");
+            Console.WriteLine("{0}", result_a);
+            Console.WriteLine("---------------");
+            ++b;
+            result_b = b.ToString();
+            Console.WriteLine("++B: ");
+            Console.WriteLine("{0}", result_b);
+            Console.WriteLine("---------------");
+            --b;
+            result_b = b.ToString();
+            Console.WriteLine("--B: ");
+            Console.WriteLine("{0}", result_b);
+
+            Console.ReadKey();
+        }
+    }
+}
